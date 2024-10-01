f8be83555e2565618ae258f749db00636bb7875e
AlphaIADA <67703831+AlphaIADA@users.noreply.github.com>
2024-10-01 07:47:32 +0100

    More

--- js/google-map.js
+++ js/google-map.js
@@ -1,62 +1,42 @@
-
 var google;
 
 function init() {
-    // Basic options for a simple Google Map
-    // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
-    // var myLatlng = new google.maps.LatLng(40.71751, -73.990922);
-    var myLatlng = new google.maps.LatLng(40.69847032728747, -73.9514422416687);
-    // 39.399872
-    // -8.224454
-    
     var mapOptions = {
-        // How zoomed in you want the map to start at (always required)
         zoom: 7,
-
-        // The latitude and longitude to center the map (always required)
-        center: myLatlng,
-
-        // How you would like to style the map. 
+        center: new google.maps.LatLng(9.0765, 7.3986), // Default center (Nigeria)
         scrollwheel: false,
-        styles: [
-            {
-                "featureType": "administrative.country",
-                "elementType": "geometry",
-                "stylers": [
-                    {
-                        "visibility": "simplified"
-                    },
-                    {
-                        "hue": "#ff0000"
-                    }
-                ]
-            }
+        styles: [ // ... (Your map styles)
         ]
     };
 
-    
+    var map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
-    // Get the HTML DOM element that will contain your map 
-    // We are using a div with id="map" seen below in the <body>
-    var mapElement = document.getElementById('map');
-
-    // Create the Google Map using out element and options defined above
-    var map = new google.maps.Map(mapElement, mapOptions);
-    
-    var addresses = ['New York'];
+    var addresses = ['Ogun State']; // Array of addresses to geocode
 
     for (var x = 0; x < addresses.length; x++) {
-        $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address='+addresses[x]+'&sensor=false', null, function (data) {
-            var p = data.results[0].geometry.location
-            var latlng = new google.maps.LatLng(p.lat, p.lng);
+        geocodeAddress(addresses[x], map);
+    }
+}
+
+function geocodeAddress(address, map) {
+    var geocoder = new google.maps.Geocoder();
+
+    geocoder.geocode({ 'address': address }, function(results, status) {
+        if (status === 'OK') {
+            var latlng = results[0].geometry.location;
+
             new google.maps.Marker({
                 position: latlng,
                 map: map,
                 icon: 'images/loc.png'
             });
 
-        });
-    }
-    
+            // Optionally, center the map on the first result
+            // map.setCenter(results[0].geometry.location);
+        } else {
+            console.error('Geocode was not successful for the following reason: ' + status);
+        }
+    });
 }
+
 google.maps.event.addDomListener(window, 'load', init);
